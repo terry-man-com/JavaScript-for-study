8e3b1278a3762bdd1029a0220ec5b385ce1667cf
terry-man-com <terry.man.com@icloud.com>
2024-11-29 10:47:35 +0900

    戻り値

--- app.js
+++ app.js
@@ -9,3 +9,28 @@ const bar = () => {
 };
 
 foo(bar);
+
+// 戻り値
+const changeDarkMode = (time) => {
+  if(new Date().getHours() > time){
+    document.body.style.backgroundColor = "#333";
+  }
+};
+const getSeason = () => {
+  const month = new Date().getMonth() + 1;
+  if(month >= 3 && month <= 5){
+    return "spring";
+  } else if (month >= 6 && month <= 8){
+    return "summer";
+  } else if (month >= 9 && month <= 11){
+    return "autumn";
+  } else {
+    return "winter";
+  }
+};
+const season = getSeason();
+if (season == "autumn"){
+  changeDarkMode(3);
+} else {
+  changeDarkMode(20);
+};
